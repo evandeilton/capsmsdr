094ae1d32819222175adb1da72b0f22272f8bf5a
evandeilton <evandeilton@gmail.com>
2018-01-03 21:08:40 -0200

    Upload travis update in README

--- R/timeline.R
+++ R/timeline.R
@@ -1,26 +1,27 @@
-#' Timeline earthquak graph
+#' Earthquak graph with timeline
 #'
-#' @param mapping Set of aethetics created by aes.
-#' @param data A data.fram such as that obtained from NOAA
-#' @param stat A string of statistical transfromation.
-#' @param position Position adjustment.
-#' @param na.rm A logical for dealing with missing values.
-#' @param show.legend A logical for showing legend or not.
-#' @param inherit.aes A logical of whether or not override default aesthetics.
-#' @param ... Other arguments passed to layer
+#' @param mapping aethetics by aes
+#' @param data A data.fram
+#' @param stat A name from a statistical transfromation
+#' @param position Position adjust
+#' @param na.rm Remove missing values
+#' @param show.legend Show legend, TRUE or FALSE
+#' @param inherit.aes Override default aesthetics, TRUE or FALSE
+#' @param ... Extra arguments
 #'
-#' @details A timeline plot is a representation of individual earthquaks ordered by their
-#'  corresponding dates as points; the color of the point represent the number of deaths
-#'  that resulted from the event and the size represents the magnitude of the event.
+#' @details A timeline plot earthquaks by dates. Colors represents number of deaths and size is the magnitude of the earthquaks.
+#' 
 #' @examples
 #' \dontrun{
-#' # load and clean data
-#' file_path <- system.file("extdata", "signif.tsv", package = "MSDR")
-#' signif <- read_tsv(file_path) %>%
+#' # Read and clean data
+#' 
+#' fp <- system.file("data", "earthquake.csv", package = "capsmsdr")
+#' 
+#' signif <- read_tsv(fp) %>%
 #'   eq_clean_date %>%
 #'   eq_clean_location
 #'
-#' # make graph
+#' # Plot
 #' signif %>%
 #' filter(COUNTRY == 'MEXICO') %>%
 #'   ggplot(aes(date = date,
@@ -44,29 +45,36 @@
 #' }
 #' @import ggplot2 dplyr grid
 #' @export
-geom_timeline <- function(mapping = NULL, data = NULL, stat = "identity",
-                          position = "identity", na.rm = FALSE, show.legend = NA,
+geom_timeline <- function(mapping = NULL,
+                          data = NULL,
+                          stat = "identity",
+                          position = "identity",
+                          na.rm = FALSE,
+                          show.legend = NA,
                           inherit.aes = TRUE, ...) {
   ggplot2::layer(
-    geom = GeomTimeline, mapping = mapping,  data = data, stat = stat,
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    geom = GeomTimeline,
+    mapping = mapping, 
+    data = data,
+    stat = stat,
+    position = position, 
+    show.legend = show.legend, 
+    inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, ...)
   )
 }
 
-#' Geom object for drawing timeline
+#' Geom configuration for ploting timeline
 #'
-#' @format NULL
-#' @usage NULL
 #' @export
 GeomTimeline <- ggplot2::ggproto("GeomTimeline",
                                  ggplot2::Geom,
                                  required_aes = c("date", "xmin", "xmax", "y", "colour", "size"),
-                                 default_aes = ggplot2::aes(shape = 19, stroke = .5, alpha = .7),
+                                 default_aes = ggplot2::aes(shape = 20, stroke = .4, alpha = .6),
                                  draw_key = function(data, params, size) {
                                    grid::pointsGrob(0.5, 0.5,
                                                     pch = data$shape,
-                                                    gp = grid::gpar(fontsize = data$size * .pt + data$stroke * .stroke / 2)
+                                                    gp = grid::gpar(fontsize = data$size * .pt + data$stroke * .stroke / 3)
                                    )
                                  },
                                  draw_panel = function(data, panel_scales, coord) {
@@ -76,47 +84,50 @@ GeomTimeline <- ggplot2::ggproto("GeomTimeline",
                                      dplyr::filter(date >= xmin, date <= xmax) %>%
                                      dplyr::mutate(x = as.numeric(date))
 
-                                   coords <- coord$transform(data, panel_scales)
+                                   coo <- coord$transform(data, panel_scales)
+                                   
                                    grid::grobTree(
                                      grid::pointsGrob(
-                                       coords$x, coords$y,
-                                       pch = coords$shape,
-                                       gp = grid::gpar(col = alpha(coords$colour, coords$alpha),
-                                                       fill = alpha(coords$fill, coords$alpha),
-                                                       fontsize = coords$size * .pt + coords$stroke * .stroke / 2)
+                                       coo$x, coo$y,
+                                       pch = coo$shape,
+                                       gp = grid::gpar(col = alpha(coo$colour, coo$alpha),
+                                                       fill = alpha(coo$fill, coo$alpha),
+                                                       fontsize = coo$size * .pt + coo$stroke * .stroke / 3)
                                      ),
                                      grid::segmentsGrob(
-                                       x0 = unit(coords$xmin, 'npc'),
-                                       x1 = unit(coords$xmax, 'npc'),
-                                       y0 = unit(coords$y, 'npc'),
-                                       y1 = unit(coords$y, 'npc'),
+                                       x0 = unit(coo$xmin, 'npc'),
+                                       x1 = unit(coo$xmax, 'npc'),
+                                       y0 = unit(coo$y, 'npc'),
+                                       y1 = unit(coo$y, 'npc'),
                                        gp = grid::gpar(col = 'gray')
                                      )
                                    )
                                  }
 )
 
-#' Timeline label
+#' Label for timeline
 #'
-#' @param mapping Set of aethetics created by aes.
-#' @param data A data.fram such as that obtained from NOAA
-#' @param stat A string of statistical transfromation.
+#' @param mapping Aethetics by aes.
+#' @param data A data.fram
+#' @param stat A name from a statistical transfromation
 #' @param position Position adjustment.
-#' @param na.rm A logical for dealing with missing values.
-#' @param show.legend A logical for showing legend or not.
-#' @param inherit.aes A logical of whether or not override default aesthetics.
-#' @param ... Other arguments passed to layer.
-#'
-#' @details Adds labels for the locations of individual earthquaks. Used with geom_timeline.
+#' @param na.rm Remove missing values
+#' @param show.legend Show legend, TRUE or FALSE
+#' @param inherit.aes Override default aesthetics, TRUE or FALSE
+#' @param ... Extra arguments
+#' 
+#' @details Labels for the locations of earthquaks.
 #' @examples
 #' \dontrun{
-#' # load and clean data
-#' file_path <- system.file("extdata", "signif.tsv", package = "MSDR")
-#' signif <- read_tsv(file_path) %>%
+#' # Read and clean data
+#' 
+#' fp <- system.file("data", "earthquake.csv", package = "capsmsdr")
+#' 
+#' signif <- read_tsv(fp) %>%
 #'   eq_clean_date %>%
 #'   eq_clean_location
 #'
-#' # make graph
+#' # Plot
 #' signif %>%
 #' filter(COUNTRY == 'MEXICO') %>%
 #'   ggplot(aes(date = date,
@@ -138,53 +149,58 @@ GeomTimeline <- ggplot2::ggproto("GeomTimeline",
 #'         panel.grid = element_blank(),
 #'         panel.background = element_blank())
 #' }
-#' @import ggplot2 dplyr grid
+#' @import dplyr grid ggplot2
 #' @export
-geom_timeline_label <- function(mapping = NULL, data = NULL, stat = "identity",
-                                position = "identity", na.rm = FALSE,
+geom_timeline_label <- function(mapping = NULL,
+                                data = NULL,
+                                stat = "identity",
+                                position = "identity", 
+                                na.rm = FALSE,
                                 show.legend = FALSE,
                                 inherit.aes = TRUE, ...) {
   ggplot2::layer(
-    geom = GeomTimelineLabel, mapping = mapping,  data = data, stat = stat,
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    geom = GeomTimelineLabel,
+    mapping = mapping,  
+    data = data, 
+    stat = stat,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, ...)
   )
 }
 
 
-#' Geom object for drawing timeline labels
+#' Geom for timeline labels
 #'
-#' @format NULL
-#' @usage NULL
 #' @export
 GeomTimelineLabel <- ggplot2::ggproto("GeomTimeline",
                                       ggplot2::Geom,
                                       required_aes = c("date", "xmin", "xmax", "location"),
                                       draw_panel = function(data, panel_params, coord) {
-                                        # prepare data
-                                        ## subset by xmin and xmax
                                         data <- data %>%
                                           dplyr::filter(date >= xmin, date <= xmax) %>%
                                           dplyr::mutate(x = as.numeric(date))
-                                        coords <- coord$transform(data, panel_params)
+                                        
+                                        coo <- coord$transform(data, panel_params)
 
-                                        txt <- grid::textGrob(label = coords$location,
-                                                              x = unit(coords$x, 'npc'),
-                                                              y = unit(coords$y + .2, 'npc'),
+                                        txt <- grid::textGrob(label = coo$location,
+                                                              x = unit(coo$x, 'npc'),
+                                                              y = unit(coo$y + .2, 'npc'),
                                                               hjust = 0,
                                                               rot = 45,
                                                               gp = grid::gpar(fontsize = 8))
-                                        segments <- grid::segmentsGrob(x0 = unit(coords$x, 'npc'),
-                                                                       x1 = unit(coords$x, 'npc'),
-                                                                       y0 = unit(coords$y, 'npc'),
-                                                                       y1 = unit(coords$y + .15, 'npc'),
+                                        segments <- grid::segmentsGrob(x0 = unit(coo$x, 'npc'),
+                                                                       x1 = unit(coo$x, 'npc'),
+                                                                       y0 = unit(coo$y, 'npc'),
+                                                                       y1 = unit(coo$y + .15, 'npc'),
                                                                        gp = grid::gpar(col = 'gray'))
                                         grid::grobTree(txt, segments)
                                       }
 )
 
 
-#' Modified theme to use with geom_timeline
+#' Extra Theme to use with geom_timeline
 theme_timeline <- ggplot2::theme(axis.line.y = ggplot2::element_blank(),
                                  axis.line.x = ggplot2::element_line(),
                                  axis.ticks.y = ggplot2::element_blank(),
